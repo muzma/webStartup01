af1f29ad24bd1f93813aa1f75eed36c909600a74
Mustofa Mahmud <mustofa_mahmud@yahoo.com>
2022-08-31 15:53:40 +0700

    Update

--- handler/user.go
+++ handler/user.go
@@ -46,3 +46,39 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	// map input dari user ke struct RegisterUserInput
 	// struct di atas kita passing sebagai parameter service
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	//user memasukkan input (email & password)
+	//input ditangkap handler
+	//mapping dari input user ke input
+	// input struct passing service
+	// di service mencari dg bantuan repository user dengan email x
+	// mencocokkan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentoken")
+	response := helper.APIResponse("Successfully Loggedin", http.StatusOK, "succsess", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
